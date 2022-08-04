6cbdda62daeb2629bafcbdaa11c2c2ccd356b8df
tmxhsk99 <tmxhsk99@naver.com>
2022-08-04 14:27:55 +0900

    feat : Order(주문) 및 OrderItem(주문 아이템)  생성 메서드 및 비즈니스 로직 추가
    1. Order 엔티티
     - 생성 메서드 추가
     비즈니스 로직
     - 주문 취소 기능 추가
     - 주문 전체 가격 조회 기능 추가
    
    2. OrderItem 엔티티
     -  생성 메서드 추가
     비즈니스 로직
     - 주문 상품 취소 기능 추가
     - 주문 상품 전체 가격 조회 기능 추가

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -10,21 +10,22 @@ import java.util.List;
 
 @Entity
 @Table(name = "orders")
-@Getter @Setter
+@Getter
+@Setter
 public class Order {
     @Id
     @GeneratedValue
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne (fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;//주문회원
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne (fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -34,18 +35,65 @@ public class Order {
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
 
     //연관관계 편의 메서드
-    public void setMember (Member member){
+    public void setMember(Member member) {
         this.member = member;
         member.getOrders().add(this);
     }
 
-    public void addOrderItem(OrderItem orderItem){
+    public void addOrderItem(OrderItem orderItem) {
         orderItems.add(orderItem);
         orderItem.setOrder(this);
     }
 
-    public void setDelivery(Delivery delivery){
+    public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    //Order 객체를 생성하기에는 여러객체를 한번에 set 해야 되기 때문에 불편하다
+    //생성 메서드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //비즈니스 로직
+
+    /**
+     * 주문취소
+     */
+    public void cancel() {
+        //주문 상태를 확인한다.
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능 합니다");
+        }
+        //주문상태를 변경한다.
+        this.setStatus(OrderStatus.CANCEL);
+        //가지고 있는 주문상품의 상태도 취소한다.
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //조회 로직
+
+    /**
+     * 전체 주문 가격 조회
+     * @return int totalPrice
+     */
+    public int getTotalPrice(){
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
 }
