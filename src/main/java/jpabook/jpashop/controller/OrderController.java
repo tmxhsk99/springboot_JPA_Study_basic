b78ba0481aaa63a48ac27f5045391dffa3135d38
tmxhsk99 <tmxhsk99@naver.com>
2022-08-08 20:53:43 +0900

    feat : 상품주문Form 추가 상문주문 취소 상품리스트 기능 추가

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
 import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.MemberService;
 import jpabook.jpashop.service.OrderService;
@@ -9,10 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,27 +25,42 @@ public class OrderController {
 
     /**
      * 멤버 폼 이동
+     *
      * @param model
      * @return
      */
     @GetMapping(value = "/order")
-    public String createForm(Model model){
+    public String createForm(Model model) {
 
         List<Member> members = memberService.findMembers();
         List<Item> items = itemService.findItems();
 
         model.addAttribute("members", members);
-        model.addAttribute("items",items);
+        model.addAttribute("items", items);
 
-        return "orders/orderForm";
+        return "order/orderForm";
     }
 
     @PostMapping(value = "/order")
-    public String order(@RequestParam("memberId") Long memberId, @RequestParam("itemId") Long itemId,@RequestParam("count") int count){
+    public String order(@RequestParam("memberId") Long memberId, @RequestParam("itemId") Long itemId, @RequestParam("count") int count) {
         log.info("memberId : " + memberId);
         log.info("itemId : " + itemId);
         log.info("count : " + count);
         orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+    @GetMapping(value = "/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+
+        return "order/orderList";
+    }
+
+    @PostMapping(value = "/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId){
+        orderService.cancelOrder(orderId);
         return "redirect:/orders";
     }
 }
