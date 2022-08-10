8051d2af9fbd5ead2947d2c1956365e85306b586
tmxhsk99 <tmxhsk99@naver.com>
2022-08-11 08:29:24 +0900

    feat : 주문 조회 API 기능 추가 :v1.엔티티 직접 컬렉션조회 , v2.DTO 컬레션조회 , v3. fech join 활용 컬렉션 조회

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jpabook.jpashop.domain.item.Item;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -21,11 +22,12 @@ public class OrderItem {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "item_ID")
+    @JoinColumn(name = "item_id")
     private Item item;
 
     @ManyToOne (fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
+    @JsonIgnore
     private Order order;
 
 
