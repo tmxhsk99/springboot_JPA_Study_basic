0a687796a61e38454142d5c9263262311d75199e
tmxhsk99 <tmxhsk99@naver.com>
2022-08-11 14:39:56 +0900

    feat : 주문조회 API v4 JPA에서 DTO 직접조회 기능추가

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Or;
@@ -22,7 +24,7 @@ import static java.util.stream.Collectors.toList;
 @RequiredArgsConstructor
 public class OrderApiController {
     private final OrderRepository orderRepository;
-
+    private final OrderQueryRepository orderQueryRepository;
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
@@ -62,7 +64,11 @@ public class OrderApiController {
         List<OrderDto> result = orders.stream().map(o -> new OrderDto(o)).collect(toList());
         return result;
     }
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4(){
 
+        return orderQueryRepository.findOrderQueryDtos();
+    }
     @Data
     static class OrderDto {
         private Long orderId;
