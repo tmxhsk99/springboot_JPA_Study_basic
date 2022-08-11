e1243405f5c17aaa389d657a6e7886d0b3b0f9ba
tmxhsk99 <tmxhsk99@naver.com>
2022-08-11 10:16:39 +0900

    feat : v3.1 엔티티를 DTO로 변환 - 페이징과 한계 돌파

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -10,6 +10,7 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -52,6 +53,14 @@ public class OrderApiController {
                 .collect(toList());
         return result;
 
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> orderV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
+                                       @RequestParam(value = "limit", defaultValue = "100") int max) {
+        List<Order> orders = orderRepository.findWithMemberDelivery(offset, max);
+        List<OrderDto> result = orders.stream().map(o -> new OrderDto(o)).collect(toList());
+        return result;
     }
 
     @Data
--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -125,4 +125,14 @@ public class OrderRepository {
                         " join fetch oi.item i",Order.class
         ).getResultList();
     }
+
+    public List<Order> findWithMemberDelivery(int offset, int limit) {
+        return em.createQuery("select o from Order o" +
+                " join fetch o.member m" +
+                " join fetch o.delivery d"
+                ,Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
 }
